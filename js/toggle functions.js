13961d29d7737b3cae9b836bbe41d5182f49a3ca
Nathan Zengamambu <zengaman@fl3-mac-09.coventry.ac.uk>
2017-12-09 02:14:16 +0000

    first commits

---
+++ js/toggle functions.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+	document.body.innerHTML = '<div class="bgvid"><video autoplay loop muted poster="https://www.dropbox.com/s/uzitgxpibq44cqh/bgvid-poster.jpg?dl=1"> <source src="https://www.dropbox.com/s/c01of0ygfrmm11m/sparks.mp4?dl=1"/> </video></div>' + document.body.innerHTML;
+
+	// if ($(".introscreen").css("display") != "none") {
+	// 	if(location.hash == "" && location.origin != document.referrer.slice(0, 22)) {
+	// 		$(".introscreen").delay( 5000 ).fadeOut( 1000, function() {
+	// 			location.hash = "#home";
+	// 		});
+	// 		$(".introscreen p:last-child").delay( 500 ).fadeIn( 500 );
+	// 	} else {
+	// 		$(".introscreen").hide(0, function() {
+	// 			location.hash = "#home";
+	// 		});
+	// 	}
+	// }
+
+	$("#dropdown").click(function() {
+		$(".nav").slideDown();
+		$(this).fadeOut();
+	});
+	$("#navclose").click(function(){
+		$(".nav").slideUp(function() {
+			$("#dropdown").fadeIn().css("display","");
+			$(this).css("display","");
+		});
+	});
+	
+	$(".contact").css({"position":"fixed" , "left":"150%"});
+	$(".about #contactOpen").click(function() {
+		$(".contact").css({"position":"" , "left":""}).fadeTo(400, 1);
+	});
+	$("#contactClose").click(function() {
+		$(".contact").fadeTo(400, 0, function() {
+			$(this).css({"position":"fixed" , "left":"150%"});
+		});
+	});
+	$(".sublist:first").css("color","gold");
+	$(".sublist").click(function(){
+		$(".sublist").css("color","");
+		$(this).css("color","gold");
+	});
+
+});
