29f5762b68da4b4a38552d807dddf8eb57ae736a
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2017-12-09 16:40:15 +0000

    updated script for bg-vid source

--- js/toggle functions.js
+++ js/toggle functions.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	document.body.innerHTML = '<div class="bgvid"><video autoplay loop muted poster="https://www.dropbox.com/s/uzitgxpibq44cqh/bgvid-poster.jpg?dl=1"> <source src="https://www.dropbox.com/s/c01of0ygfrmm11m/sparks.mp4?dl=1"/> </video></div>' + document.body.innerHTML;
+	document.body.innerHTML = '<div class="bgvid"><video autoplay loop muted poster="/img/bgvid-poster.jpg"> <source src="/img/sparks.mp4"/> </video></div>' + document.body.innerHTML;
 
 	// if ($(".introscreen").css("display") != "none") {
 	// 	if(location.hash == "" && location.origin != document.referrer.slice(0, 22)) {
