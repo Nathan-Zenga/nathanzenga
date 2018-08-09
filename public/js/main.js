a289b3aeb88f68c9bc8d5ada2d0edd0b6757dd0a
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2018-08-09 22:13:24 +0100

    initial commits for version 6

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,53 @@
-$(document).ready(function() {
+$(function() {
+	$("nav .link").click(function() {
+		try {
+			var s = this.id;
+			$("html, body").stop().animate({
+				scrollTop: $("section." + s).offset().top
+			}, 500)
+		} catch(err) {
+			console.log("Section doesn't exist")
+		}
+		if (window.innerWidth < 768) $("#navclose").click();
+	});
 
-	$("#dropdown").click(function() {
-		$(".nav").slideDown();
+	$("#nav_dropdown").click(function() {
+		$("nav").slideDown();
 		$(this).fadeOut();
 	});
+
 	$("#navclose").click(function(){
-		$(".nav").slideUp(function() {
-			$("#dropdown").fadeIn().css("display","");
+		$("nav").slideUp(function() {
+			$("#nav_dropdown").fadeIn().css("display","");
 			$(this).css("display","");
 		});
 	});
-	
-	$(".contact").css({"position":"fixed" , "left":"150%"});
-	$(".about #contactOpen").click(function() {
-		$(".contact").css({"position":"" , "left":""}).fadeTo(400, 1);
-	});
-	$("#contactClose").click(function() {
-		$(".contact").fadeTo(400, 0, function() {
-			$(this).css({"position":"fixed" , "left":"150%"});
+
+	$(".contact .submit").click(function(e){
+		e.preventDefault();
+
+		var $details = $(this).closest("form").find(".details");
+		var data = {};
+
+		$details.each(function() {
+			var key = $(this).attr('name');
+			data[key] = $(this).val();
 		});
-	});
-	$(".sublist:first").css("color","gold");
-	$(".sublist").click(function(){
-		$(".sublist").css("color","");
-		$(this).css("color","gold");
+
+		$.post('/send/message', data, function(msg, status) {
+			if (status !== 'success') {
+				alert("error: "+status );
+			} else {
+				$(".contact form")
+					.find(".details")
+					.val("")
+					.end()
+					.find(".result")
+					.css("display", "none")
+					.html("<h3 style='text-align:center'>"+ msg +"</h3>")
+					.slideDown().delay(3000).slideUp();
+			}
+		});
 	});
 
 });
--- public/js/nz-gal.js
+++ public/js/nz-gal.js
@@ -1,62 +1,58 @@
 $(function() {
-	Galleria.loadTheme("https://cdnjs.cloudflare.com/ajax/libs/galleria/1.5.7/themes/classic/galleria.classic.min.js").configure("transition", "fade");
+	Galleria.loadTheme("https://cdnjs.cloudflare.com/ajax/libs/galleria/1.5.7/themes/classic/galleria.classic.min.js").configure({transition: "fade"});
 	var g = {
-		abstract: "72157663613653419",
-		figures: "72157663122488679",
-		vandalism: "72157656087051975",
-		ravensbourne: "72157656059298655",
-		aplha: "72157652167823921",
-		places1: "72157647897931254",
-		places2: "72157680342839503",
-		fest: "72157647897269204",
-		holga: "72157646711213358",
-		assorted: "72157647107363402",
-		interval: "72157647107024312",
-		royal: "72157647058898826",
-		solarised: "72157647119896135",
-		light: "72157647106866472",
-		passive: "72157647058775116",
-		incognito: "72157647058547856",
-		art: "72157646698234267",
-		cyborg: "72157646697855169",
-		crest: "72157646698041797",
-		designs: "72157646703093220",
-		closed: "72157646697568489",
-		fauna: "72157647905448693",
-		"4svciety": "72157682756177700",
-		eddygrant: "72157681869499180",
-		beatfreeks: "72157680397025793",
-		scanned: "72157663115670011",
-		kojey: "gejHlqqRoAM",
-		trip: "qWoNMJ0HeaY",
-		dede: "dYSLvmaZlh0",
-		ab: "KkCTDiIECM8",
-		laswap: "jYZiBss_eSU"
+		abstract: {set: "72157663613653419", t: "abstract"},
+		figures: {set: "72157663122488679", t: "figures"},
+		vandalism: {set: "72157656087051975", t: "vandalism"},
+		ravensbourne: {set: "72157656059298655", t: "ravensbourne"},
+		aplha: {set: "72157652167823921", t: "aplha"},
+		places1: {set: "72157647897931254", t: "places1"},
+		places2: {set: "72157680342839503", t: "places2"},
+		fest: {set: "72157647897269204", t: "fest"},
+		holga: {set: "72157646711213358", t: "holga"},
+		assorted: {set: "72157647107363402", t: "assorted"},
+		interval: {set: "72157647107024312", t: "interval"},
+		royal: {set: "72157647058898826", t: "royal"},
+		solarised: {set: "72157647119896135", t: "solarised"},
+		light: {set: "72157647106866472", t: "light"},
+		passive: {set: "72157647058775116", t: "passive"},
+		incognito: {set: "72157647058547856", t: "incognito"},
+		art: {set: "72157646698234267", t: "art"},
+		cyborg: {set: "72157646697855169", t: "cyborg"},
+		crest: {set: "72157646698041797", t: "crest"},
+		designs: {set: "72157646703093220", t: "designs"},
+		closed: {set: "72157646697568489", t: "closed"},
+		fauna: {set: "72157647905448693", t: "fauna"},
+		"4svciety": {set: "72157682756177700", t: "4svciety"},
+		eddygrant: {set: "72157681869499180", t: "eddygrant"},
+		beatfreeks: {set: "72157680397025793", t: "beatfreeks"},
+		scanned: {set: "72157663115670011", t: "scanned"},
+		kojey: {set: "gejHlqqRoAM", t: "kojey"},
+		trip: {set: "qWoNMJ0HeaY", t: "trip"},
+		dede: {set: "dYSLvmaZlh0", t: "dede"},
+		ab: {set: "KkCTDiIECM8", t: "ab"},
+		laswap: {set: "jYZiBss_eSU", t: "laswap"}
 	};
 
 	var imageSize = 'big';
-	var id = $(".iframe").attr("id");
+	var imageCrop = window.innerWidth < 768 ? false : true;
+	var id = $(".index-slideshow").attr("id");
 
-	if ($('#video-page, #about-page').length == 0) {
-		Galleria.run( "#"+id, { flickr:"set:"+g[id], flickrOptions: { imageSize: imageSize } })
+	if ($(".index-slideshow").length) {
+		if (id == 'assorted') {
+			Galleria.run( "#"+id, { imageCrop: imageCrop, flickr:"set:"+g[id].set, flickrOptions: { imageSize: imageSize }, autoplay: 3000 })
+		} else {
+			Galleria.run( "#"+id, { imageCrop: imageCrop, flickr:"set:"+g[id].set, flickrOptions: { imageSize: imageSize }})
+		}
 	}
 
-	$(".sublist a").click(function() {
-		if ($(window).width() < 768) {
-			$("#navclose").click();
-		}
-
-		var id = this.id.slice(0,-1);
-		var e = '<div class="iframe" oncontextmenu="return false" id="' + id + '"></div>';
-		var vid = '<iframe class="iframe" src="https://www.youtube.com/embed/' + g[id] +
-		'?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>';
-
-		if( $("#video-page").length ) {
-			$(".main span").html(vid);
-
-		} else {
-			$(".main span").html(e);
-			Galleria.run( "#"+id, { flickr:"set:"+g[id], flickrOptions: { imageSize: imageSize } });
+	$(".work .thumb").each(function(){
+		try {
+			var id = this.id;
+			Galleria.run( ".work .thumb#"+ id +" .cover-image", { flickr:"search:nz-"+g[id].t+"-cover", flickrOptions: { max: 1 }, imageCrop: true })
+		} catch(err) {
+			console.log(err)
 		}
 	});
+
 });
