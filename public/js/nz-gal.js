518e61044290e85de3d0949a83a7a36b0c120568
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2018-08-10 17:48:47 +0100

    nodemailer + minor changes

--- public/js/nz-gal.js
+++ public/js/nz-gal.js
@@ -1,37 +1,37 @@
 $(function() {
 	Galleria.loadTheme("https://cdnjs.cloudflare.com/ajax/libs/galleria/1.5.7/themes/classic/galleria.classic.min.js").configure({transition: "fade"});
 	var g = {
-		abstract: {set: "72157663613653419", t: "abstract"},
-		figures: {set: "72157663122488679", t: "figures"},
-		vandalism: {set: "72157656087051975", t: "vandalism"},
-		ravensbourne: {set: "72157656059298655", t: "ravensbourne"},
-		aplha: {set: "72157652167823921", t: "aplha"},
-		places1: {set: "72157647897931254", t: "places1"},
-		places2: {set: "72157680342839503", t: "places2"},
-		fest: {set: "72157647897269204", t: "fest"},
-		holga: {set: "72157646711213358", t: "holga"},
-		assorted: {set: "72157647107363402", t: "assorted"},
-		interval: {set: "72157647107024312", t: "interval"},
-		royal: {set: "72157647058898826", t: "royal"},
-		solarised: {set: "72157647119896135", t: "solarised"},
-		light: {set: "72157647106866472", t: "light"},
-		passive: {set: "72157647058775116", t: "passive"},
-		incognito: {set: "72157647058547856", t: "incognito"},
-		art: {set: "72157646698234267", t: "art"},
-		cyborg: {set: "72157646697855169", t: "cyborg"},
-		crest: {set: "72157646698041797", t: "crest"},
-		designs: {set: "72157646703093220", t: "designs"},
-		closed: {set: "72157646697568489", t: "closed"},
-		fauna: {set: "72157647905448693", t: "fauna"},
-		"4svciety": {set: "72157682756177700", t: "4svciety"},
-		eddygrant: {set: "72157681869499180", t: "eddygrant"},
-		beatfreeks: {set: "72157680397025793", t: "beatfreeks"},
-		scanned: {set: "72157663115670011", t: "scanned"},
-		kojey: {set: "gejHlqqRoAM", t: "kojey"},
-		trip: {set: "qWoNMJ0HeaY", t: "trip"},
-		dede: {set: "dYSLvmaZlh0", t: "dede"},
-		ab: {set: "KkCTDiIECM8", t: "ab"},
-		laswap: {set: "jYZiBss_eSU", t: "laswap"}
+		abstract: {set: "72157663613653419", tag: "abstract"},
+		figures: {set: "72157663122488679", tag: "figures"},
+		vandalism: {set: "72157656087051975", tag: "vandalism"},
+		ravensbourne: {set: "72157656059298655", tag: "ravensbourne"},
+		aplha: {set: "72157652167823921", tag: "aplha"},
+		places1: {set: "72157647897931254", tag: "places1"},
+		places2: {set: "72157680342839503", tag: "places2"},
+		fest: {set: "72157647897269204", tag: "fest"},
+		holga: {set: "72157646711213358", tag: "holga"},
+		assorted: {set: "72157647107363402", tag: "assorted"},
+		interval: {set: "72157647107024312", tag: "interval"},
+		royal: {set: "72157647058898826", tag: "royal"},
+		solarised: {set: "72157647119896135", tag: "solarised"},
+		light: {set: "72157647106866472", tag: "light"},
+		passive: {set: "72157647058775116", tag: "passive"},
+		incognito: {set: "72157647058547856", tag: "incognito"},
+		art: {set: "72157646698234267", tag: "art"},
+		cyborg: {set: "72157646697855169", tag: "cyborg"},
+		crest: {set: "72157646698041797", tag: "crest"},
+		designs: {set: "72157646703093220", tag: "designs"},
+		closed: {set: "72157646697568489", tag: "closed"},
+		fauna: {set: "72157647905448693", tag: "fauna"},
+		"4svciety": {set: "72157682756177700", tag: "4svciety"},
+		eddygrant: {set: "72157681869499180", tag: "eddygrant"},
+		beatfreeks: {set: "72157680397025793", tag: "beatfreeks"},
+		scanned: {set: "72157663115670011", tag: "scanned"},
+		kojey: {set: "gejHlqqRoAM", tag: "kojey"},
+		trip: {set: "qWoNMJ0HeaY", tag: "trip"},
+		dede: {set: "dYSLvmaZlh0", tag: "dede"},
+		ab: {set: "KkCTDiIECM8", tag: "ab"},
+		laswap: {set: "jYZiBss_eSU", tag: "laswap"}
 	};
 
 	var imageSize = 'big';
@@ -49,7 +49,7 @@ $(function() {
 	$(".work .thumb").each(function(){
 		try {
 			var id = this.id;
-			Galleria.run( ".work .thumb#"+ id +" .cover-image", { flickr:"search:nz-"+g[id].t+"-cover", flickrOptions: { max: 1 }, imageCrop: true })
+			Galleria.run( ".work .thumb#"+ id +" .cover-image", { flickr:"search:nz-"+g[id].tag+"-cover", flickrOptions: { max: 1 }, imageCrop: true })
 		} catch(err) {
 			console.log(err)
 		}
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-	router = express.Router();
+	router = express.Router(),
+	nodemailer = require('nodemailer');
 
 router.get('/', (req, res) => {
 	res.render('index', {
@@ -10,8 +11,33 @@ router.get('/', (req, res) => {
 });
 
 router.post('/send/message', (req, res) => {
-	console.log(req.body);
-	res.send("Messege Sent");
+	let transporter = nodemailer.createTransport({
+		service: 'gmail',
+		port: 465,
+		secure: true,
+		auth: {
+			user: 'nznodemailer@gmail.com',
+			pass: 'nodemailer246'
+		},
+		tls: {
+			rejectUnauthorized: true
+		}
+	});
+
+	let mailOptions = {
+		from: `"${req.body.name}" <${req.body.email}>`,
+		to: 'nathanzenga@gmail.com',
+		subject: req.body.subject,
+		text: req.body.message
+	};
+
+	transporter.sendMail(mailOptions, function(err, info) {
+		if (err) return console.log(err);
+		console.log("The message was sent!");
+		console.log(info);
+		console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+		res.send('Message Sent');
+	});
 });
 
 module.exports = router;
