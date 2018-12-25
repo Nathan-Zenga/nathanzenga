e91482df3fc046a741ecd837e59bc644f1d02626
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2018-12-25 15:02:08 +0000

    refreshing slideshow on screen rotation

--- public/js/nz-gal.js
+++ public/js/nz-gal.js
@@ -1,27 +1,35 @@
 $(function() {
+	Galleria.loadTheme("https://cdnjs.cloudflare.com/ajax/libs/galleria/1.5.7/themes/classic/galleria.classic.min.js").configure({ transition: 'fade' });
 	$.get('/get/galleries', function(g, status) {
 		function isDevice (rgx) { return rgx.test(detect.parse(navigator.userAgent).device.type) };
 		function swipe () { return isDevice(/Mobile|Tablet/) ? 'auto' : 'disabled' };
 		var imageSize = 'big';
 		var imageCrop = isDevice(/Mobile|Tablet/) || window.innerWidth < 768 ? false : true;
+		var runSlideshow = () => {
+			return Galleria.run( "#assorted", {
+				imageCrop: (window.innerWidth >= window.innerHeight) || imageCrop,
+				flickr: "set:"+g.assorted.set,
+				flickrOptions: { imageSize: imageSize },
+				autoplay: 3000,
+				swipe: "disabled"
+			})
+		};
 
-		Galleria
-		.loadTheme("https://cdnjs.cloudflare.com/ajax/libs/galleria/1.5.7/themes/classic/galleria.classic.min.js")
-		.configure({ transition: 'fade' })
-		.run( "#assorted", { imageCrop: imageCrop, flickr:"set:"+g.assorted.set, flickrOptions: { imageSize: imageSize }, autoplay: 3000, swipe: "disabled" });
+		runSlideshow();
+		window.addEventListener("orientationchange", runSlideshow);
 
 		$(".work .thumb")
 		.each(function() {
 			try {
 				var id = this.id;
-				Galleria.run( ".work .thumb#"+ id +" .cover-image", { flickr:"search:nz-"+g[id].tag+"-cover", flickrOptions: { max: 1 }, imageCrop: true })
+				Galleria.run( ".work .thumb#"+ id +" .cover-image", { flickr: "search:nz-"+g[id].tag+"-cover", flickrOptions: { max: 1 }, imageCrop: true })
 			} catch(err) {
 				console.log(err)
 			}
 		})
 		.click(function() {
 			var id = this.id;
-			Galleria.run( "#gallery_view .iframe", { flickr:"set:"+g[id].set, flickrOptions: { imageSize: imageSize }, swipe: swipe() });
+			Galleria.run( "#gallery_view .iframe", { flickr: "set:"+g[id].set, flickrOptions: { imageSize: imageSize }, swipe: swipe() });
 		});
 	});
 });
