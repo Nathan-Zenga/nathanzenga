dfb6cbf52d95fe6553a25da2d2517d1c46efffea
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2019-03-08 10:56:12 +0000

    modifying keys object properties; toggleClass function for onload + onscroll events

--- public/js/main.js
+++ public/js/main.js
@@ -11,24 +11,9 @@ $(function() {
 		return arr;
 	};
 
-	$(".menu-icon").click(function() {
-		$(this).toggleClass("is-active");
-		$("nav").slideToggle(function() {
-			if ($(this).css("display") == "none") $(this).css("display", "");
-		});
-		return;
-	});
-
 	var keys = {
-		// 32: 1, // spacebar
-		// 33: 1, // pageup
-		// 34: 1, // pagedown
-		// 35: 1, // end
-		// 36: 1, // home
 		37: 1, // left
-		// 38: 1, // up
-		39: 1, // right
-		// 40: 1  // down
+		39: 1 // right
 	};
 
 	// var smoothScroll = (scrollTop) => {
@@ -65,86 +50,20 @@ $(function() {
 		}
 	}
 
+	var toggleClass = function() {
+		$(".menu").toggleClass("fixed", window.pageYOffset >= $(".row.menu").offset().top - 20);
+	};
+
+	$(".menu-icon").click(function() {
+		$(this).toggleClass("is-active");
+		$("nav").stop().slideToggle(function() {
+			if ($(this).css("display") == "none") $(this).css("display", "");
+		});
+		return;
+	});
+
 	document.onkeydown = overrideKeyPress;
+	toggleClass();
+	$(window).scroll(toggleClass);
 
-	// $("input:not(:submit), textarea")
-	// 	.focusin(function(){
-	// 		document.onkeydown = null;
-	// 	})
-	// 	.focusout(function(){
-	// 		document.onkeydown = overrideKeyPress;
-	// 	});
-
-	// // for mouse wheel/touchpad movements
-	// $('html, body').on('DOMMouseScroll mousewheel', function(e){
-	// 	function main() {
-	// 		e = e || window.event;
-	// 		if (e.preventDefault) e.preventDefault();
-
-	// 		// scroll down, else scroll up
-	// 		e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0 ? index += 1 : index -= 1;
-
-	// 		if (index < 0) index = 0;
-	// 		else if ( index > $("section:last").index() ) index = $("section:last").index();
-
-	// 		smoothScroll($("section").eq(index).offset().top);
-	// 	}
-
-	// 	$(":animated").length ? this.addEventListener("animationend", main) : main();
-
-	// 	//prevent page from scrolling
-	// 	return false;
-	// });
-
-	// $("nav .link").click(function() {
-	// 	try {
-	// 		var s = this.id;
-	// 		index = $("section." + s).index();
-	// 		smoothScroll($("section." + s).offset().top)
-	// 	} catch(err) {
-	// 		console.log("Section doesn't exist")
-	// 	}
-	// 	if (window.innerWidth < 768) $("#navclose").click();
-	// });
-
-	// $("#nav_dropdown").click(function() {
-	// 	$("nav").slideDown();
-	// 	$(this).fadeOut();
-	// });
-
-	// $("#navclose").click(function(){
-	// 	$("nav").slideUp(function() {
-	// 		$("#nav_dropdown").fadeIn().css("display","");
-	// 		$(this).css("display","");
-	// 	});
-	// });
-
-	// $(".contact .submit").click(function(e){
-	// 	e.preventDefault();
-
-	// 	$(".contact .result")
-	// 	.css("display", "none")
-	// 	.html("<p style='text-align:center; margin-top: 20px'>Sending...</p>")
-	// 	.fadeIn();
-
-	// 	var $details = $(this).closest("form").find(".details");
-	// 	var data = {};
-
-	// 	$details.each(function() {
-	// 		var key = $(this).attr('name');
-	// 		data[key] = $(this).val();
-	// 	});
-
-	// 	$.post('/send/message', data, function(msg, status) {
-	// 		if (status !== 'success') {
-	// 			alert("error: "+status );
-	// 		} else {
-	// 			if (msg.includes("sent")) $(".contact form .details").val("");
-	// 			$(".contact .result")
-	// 				.stop()
-	// 				.fadeOut(function() { $(this).html("<h3 style='text-align:center'>"+ msg +"</h3>") })
-	// 				.fadeIn().delay(3000).fadeOut();
-	// 		}
-	// 	});
-	// });
 });
--- public/js/nz-gal.js
+++ public/js/nz-gal.js
@@ -1,34 +1,7 @@
 $(function() {
 	Galleria.loadTheme("https://cdnjs.cloudflare.com/ajax/libs/galleria/1.5.7/themes/classic/galleria.classic.min.js").configure({ transition: 'fade' });
 	$.get('/get/galleries', function(g, status) {
-		function isDevice (rgx) { return rgx.test(detect.parse(navigator.userAgent).device.type) };
-		function swipe () { return isDevice(/Mobile|Tablet/) ? 'auto' : 'disabled' };
 		var imageSize = 'big';
-		// var isLandscape;
-		// var toToggle = () => {
-		// 	if (isLandscape != (window.innerWidth >= window.innerHeight)) {
-		// 		isLandscape = (window.innerWidth >= window.innerHeight);
-		// 		Galleria.run( "#assorted", {
-		// 			imageCrop: isLandscape,
-		// 			flickr: "set:"+g.assorted.set,
-		// 			flickrOptions: { imageSize: imageSize },
-		// 			autoplay: 3000,
-		// 			showImagenav: false,
-		// 			showInfo: false,
-		// 			thumbnails: false,
-		// 			swipe: "disabled"
-		// 		})
-		// 	}
-
-		// 	if (window.innerWidth < 768) {
-		// 		$(".thumbs-modal").each(function(){
-		// 			let length = $(this).find(".thumb-img").length;
-		// 			$(this).find(".thumb-img").css("height", length >= 4 ? "calc(25vh - 20px)" : "calc(" + (100 / length) + "vh - 20px)");
-		// 		});
-		// 	} else {
-		// 		$(".thumbs-modal .thumb-img").css("height", "");
-		// 	}
-		// };
 
 		$(".content.grid > .thumb > .inner").each(function(i){
 			$(this).galleria({
