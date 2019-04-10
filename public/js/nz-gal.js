26f20f5db9371de84b49b85e0060d42cbb198d8b
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2019-04-10 16:55:34 +0100

    commits for photography page

--- public/js/nz-gal.js
+++ public/js/nz-gal.js
@@ -3,16 +3,18 @@ $(function() {
 	$.get('/get/galleries', function(g, status) {
 		var imageSize = 'big';
 
-		$(".content.grid > .thumb > .inner").each(function(i){
+		$(".content.grid > .thumb > .inner").each(function(i) {
+			var id = this.id;
+			var flickr = id ? "search:nz-" + g[id].tag + "-cover" : "set:" + g.assorted.set
 			$(this).galleria({
 				imageCrop: true,
-				flickr: "set:" + g.assorted.set,
+				flickr: flickr,
 				flickrOptions: { imageSize: imageSize },
 				showImagenav: false,
 				showInfo: false,
 				thumbnails: false,
 				swipe: "disabled",
-				show: i,
+				show: id ? 0 : i,
 				max: 1
 			})
 		}).click(function() {
