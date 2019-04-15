18cde76e515371e502b5ef47c5f8b3617a4f001b
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2019-04-15 20:27:47 +0100

    modified click event code for activated Galleria instances in both home + photography pages

--- public/js/nz-gal.js
+++ public/js/nz-gal.js
@@ -3,7 +3,7 @@ $(function() {
 	$.get('/get/galleries', function(g, status) {
 		var imageSize = 'big';
 
-		$(".content.grid > .thumb > .inner").each(function(i) {
+		$(".content.galleria-init > .img-container > .inner").each(function(i) {
 			var id = this.id;
 			var flickr = id ? "search:nz-" + g[id].tag + "-cover" : "set:" + g.assorted.set
 			$(this).galleria({
@@ -13,20 +13,29 @@ $(function() {
 				showImagenav: false,
 				showInfo: false,
 				thumbnails: false,
-				swipe: "disabled",
 				show: id ? 0 : i,
 				max: 1
 			})
+
 		}).click(function() {
-			var i = $(".content.grid > .thumb > .inner").index(this);
-			$("#gallery_view .iframe").galleria({
-				flickr: "set:" + g.assorted.set,
+			var i = $(".content.grid > .img-container > .inner").index(this);
+			var id = this.id;
+			var options = {
 				flickrOptions: { imageSize: imageSize },
 				showInfo: false,
 				thumbnails: false,
-				show: i,
-				max: 1
-			})
+				swipe: "disabled"
+			};
+
+			if (document.body.id == "home") {
+				options.flickr = "set:" + g.assorted.set;
+				options.show = i;
+				options.max = 1;
+			} else if (document.body.id == "photo") {
+				options.flickr = "set:" + g[id].set;
+			}
+
+			$("#gallery_view .iframe").galleria(options);
 		})
 	});
 });
