873b09e4c396642ad072019291a6016ef38cf27a
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2019-06-07 17:26:07 +0100

    refactoring for dynamic population of Galleria instances (i.e. image thumbs in homepage)

--- public/js/nz-gal.js
+++ public/js/nz-gal.js
@@ -3,57 +3,71 @@ $(function() {
 	$.post('/galleries', function(g, status) {
 		var imageSize = 'big';
 		var isSlideshow;
+		var pagename = document.body.id;
+		var flickr = new Galleria.Flickr(g.key);
 
-		$(".content.galleria-init .img").each(function(i) {
-			var img = this;
-			var imgID = img.id;
-			isSlideshow = this.className.includes("slideshow");
+		flickr.setOptions({ max: Infinity });
 
-			if (document.body.id === "photo") {
-				let flickr = new Galleria.Flickr(g.key);
-
-				flickr.setOptions({
-					max: Infinity
-				}).tags("nz-"+ g.sets[imgID].tag +"-cover", function(data) {
-					$(img).html("<img src="+ data[0].big +">").galleria({
-						imageCrop: true,
-						showImagenav: false
-					})
-				});
-			} else {
-				let flickr;
-				if      (document.body.id === "home")     flickr = "set:" + g.sets.assorted.set;
-				else if (document.body.id === "design")   flickr = "tags:nz-designs-" + imgID;
-
-				$(this).galleria({
-					imageCrop: !isSlideshow,
-					flickr: flickr,
-					flickrOptions: { imageSize: isSlideshow ? "original" : imageSize },
-					showImagenav: false,
-					show: imgID ? 0 : i,
-					max: isSlideshow ? undefined : 1,
-					autoplay: isSlideshow ? 4000 : undefined,
-					swipe: isSlideshow ? "disabled" : undefined
-				})
-			}
-
-		}).click(function() {
+		function onclick(e) {
+			var isSlideshow = e.data ? e.data.isSlideshow : null;
 			var i = $(".content.galleria-init .img").index(this);
 			var id = this.id;
 			var options = {
 				flickrOptions: { imageSize: isSlideshow ? "original" : imageSize }
 			};
 
-			if (document.body.id == "home") {
+			if (pagename == "home") {
 				options.flickr = "set:" + g.sets.assorted.set;
 				options.show = i;
-			} else if (document.body.id == "photo") {
+			} else if (pagename == "photo") {
 				options.flickr = "set:" + g.sets[id].set;
-			} else if (document.body.id == "design") {
+			} else if (pagename == "design") {
 				options.flickr = "tags:nz-designs-" + id;
 			}
 
 			$("#gallery_view .iframe").galleria(options);
-		})
+		}
+
+		if (pagename === "home") {
+			flickr.set(g.sets.assorted.set, function(data) {
+				data.forEach(function(img, i) {
+					$(".content.galleria-init")
+					.append('<div class="col-sm-6 col-md-4 img-container"><div class="inner img" data-toggle="modal" data-target="#gallery_view" oncontextmenu="return false"><img src="'+ img.big +'"></div></div>')
+					.find(".img").eq(-1)
+					.galleria({
+						imageCrop: true,
+						showImagenav: false
+					});
+					if (i === data.length-1) $(".content.galleria-init .img").click(onclick);
+				})
+			});
+		} else {
+			$(".content.galleria-init .img").each(function(i) {
+				var img = this;
+				var imgID = img.id;
+				isSlideshow = this.className.includes("slideshow");
+
+				if (pagename === "photo") {
+					flickr.tags("nz-"+ g.sets[imgID].tag +"-cover", function(data) {
+						$(img).html("<img src="+ data[0].big +">").galleria({
+							imageCrop: true,
+							showImagenav: false
+						})
+					});
+				}
+				else if (pagename === "design") {
+					$(this).galleria({
+						imageCrop: !isSlideshow,
+						flickr: "tags:nz-designs-" + imgID,
+						flickrOptions: { imageSize: isSlideshow ? "original" : imageSize },
+						showImagenav: false,
+						show: imgID ? 0 : i,
+						max: isSlideshow ? undefined : 1,
+						autoplay: isSlideshow ? 4000 : undefined,
+						swipe: isSlideshow ? "disabled" : undefined
+					})
+				}
+			}).click(isSlideshow, onclick)
+		}
 	});
 });
