b8ebbda002df2f49ce8446b3d081b6c7da7d9ed6
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2019-08-21 14:48:00 +0100

    import schemas for page rendering + test POST request for updating custom settings

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,61 @@
-var express = require('express'),
-	router = express.Router(),
-	galleries = require('../config/galleries'),
-	nodemailer = require('nodemailer');
+var express = require('express');
+var router = express.Router();
+var galleries = require('../config/galleries');
+var nodemailer = require('nodemailer');
+var models = require('../models/models');
 
 router.get('/', (req, res) => {
 	res.render('index', { title: null, pagename: "home" })
 });
 
 router.get('/photo', (req, res) => {
-	res.render('photo', { title: "Photography", pagename: "photo" })
+	models.gallery.find((err, docs) => {
+		res.render('photo', { title: "Photography", pagename: "photo", docs })
+	})
 });
 
 router.get('/design', (req, res) => {
-	res.render('design', { title: "Designs", pagename: "design" })
+	models.design.find((err, docs) => {
+		res.render('design', { title: "Designs", pagename: "design", docs })
+	})
 });
 
 router.get('/info', (req, res) => {
-	res.render('info', { title: "Info", pagename: "info" })
+	models.info_text.find((err, docs) => {
+		res.render('info', { title: "Info", pagename: "info", docs })
+	})
+});
+
+router.get('/---settings---', (req, res) => {
+	var allDocs = [];
+	models.gallery.find((err, docs) => {
+		allDocs = allDocs.concat(docs);
+
+		models.design.find((err, docs) => {
+			allDocs = allDocs.concat(docs);
+
+			models.info_text.find((err, docs) => {
+				allDocs = allDocs.concat(docs);
+				res.render('settings', { title: "Settings", pagename: "settings", allDocs })
+			})
+		})
+	})
 });
 
 router.post('/galleries', (req, res) => {
 	res.send(galleries);
+});
+
+router.post('/save', (req, res) => {
+	var newGallery = new models.gallery({
+		gallery: req.body.gallery,
+		label: req.body.label
+	});
+
+	newGallery.save(err => {
+		if (err) return res.send(err);
+		res.redirect(req.get("referer"));
+	})
 });
 
 router.post('/send/message', (req, res) => {
@@ -44,7 +79,7 @@ router.post('/send/message', (req, res) => {
 		text: `From ${req.body.name} (${req.body.email}):\n\n${req.body.message}`
 	};
 
-	transporter.sendMail(mailOptions, function(err, info) {
+	transporter.sendMail(mailOptions, (err, info) => {
 		if (err) return console.log(err), res.send("Could not send message");
 		console.log("The message was sent!");
 		console.log(info);
