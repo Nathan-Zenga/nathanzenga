4b91103af810dd3a7a8d4fd4b91cb9c04c785407
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2019-08-21 15:44:08 +0100

    reformatting schema property

--- models/models.js
+++ models/models.js
@@ -7,8 +7,12 @@ module.exports.gallery = mongoose.model('Gallery', mongoose.Schema({
 
 module.exports.design = mongoose.model('Design', mongoose.Schema({
 	id: String,
-	link: String,
-	text: String
+	text: {
+		client: String,
+		tools: String,
+		description: String
+	},
+	link: String
 }));
 
 module.exports.info_text = mongoose.model('Info-Text', mongoose.Schema({
