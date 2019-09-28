6fd22762493f74286c4e0d6d9cd46e0200258752
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2019-09-28 21:18:08 +0100

    distinguishing post requests for settings page, starting with 'save/gallery' process supporting bulk input of galleries list

--- routes/settings.js
+++ routes/settings.js
@@ -18,39 +18,66 @@ router.get('/---', (req, res) => {
 	})
 });
 
-router.post('/save/:setting', (req, res) => {
-	switch (req.params.setting) {
+router.post('/save/gallery', (req, res) => {
+	var obj;
+	var complete = obj => {
+		models.gallery.collection.insert(obj, err => {
+			if (err) return res.send(err);
+			res.redirect(req.get("referrer"));
+		})
+	};
 
-		case "gallery":
-			var newGallery = new models.gallery({
-				gallery: req.body.gallery,
-				label: req.body.label
+	if (req.body.bulk) {
+		models.gallery.collection.deleteMany({}, err => {
+			let bulk = req.body.bulk.split("\n").map(gallery => {
+				let temp = {};
+				let e = gallery.split(" - ");
+				temp[e[0]] = e[1];
+				return temp
 			});
-			newGallery.save(err => { if (err) return res.send(err) })
-			break;
-
-		case "designs":
-			var newDesign = new models.design({
-				id: req.body.id,
-				text: {
-					client: req.body.client,
-					tools: req.body.tools,
-					description: req.body.description
-				},
-				link: req.body.link
-			});
-			newDesign.save(err => { if (err) return res.send(err) })
-			break;
-
-		case "info-text":
-			var newInfo = new models.design({
-				info: req.body.info
-			});
-			newInfo.save(err => { if (err) return res.send(err) })
-			break;
+			obj = bulk;
+			complete(obj);
+		})
+	} else {
+		obj = { gallery: req.body.gallery, label: req.body.label };
+		complete([obj]);
 	}
-	res.redirect(req.get("referrer"));
 });
+
+
+// router.post('/save/:setting', (req, res) => {
+// 	switch (req.params.setting) {
+
+// 		case "gallery":
+// 			var newGallery = new models.gallery({
+// 				gallery: req.body.gallery,
+// 				label: req.body.label
+// 			});
+// 			newGallery.save(err => { if (err) return res.send(err) })
+// 			break;
+
+// 		case "designs":
+// 			var newDesign = new models.design({
+// 				id: req.body.id,
+// 				text: {
+// 					client: req.body.client,
+// 					tools: req.body.tools,
+// 					description: req.body.description
+// 				},
+// 				link: req.body.link
+// 			});
+// 			newDesign.save(err => { if (err) return res.send(err) })
+// 			break;
+
+// 		case "info-text":
+// 			var newInfo = new models.design({
+// 				info: req.body.info
+// 			});
+// 			newInfo.save(err => { if (err) return res.send(err) })
+// 			break;
+// 	}
+// 	res.redirect(req.get("referrer"));
+// });
 
 router.post('/delete/:section/:id', (req, res) => {
 	models[req.params.section].deleteOne({_id: req.params.id}, err => {
