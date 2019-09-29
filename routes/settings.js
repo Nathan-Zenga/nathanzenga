dd7887b94c9f5efb33f951c385696b6e3f9709d8
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2019-09-29 19:25:43 +0100

    re-modifying form emptiness validation - toggling requirement of fields depending on population of 'bulk' field; shorthand refactoring on settings routes file

--- routes/settings.js
+++ routes/settings.js
@@ -4,14 +4,9 @@ var nodemailer = require('nodemailer');
 var models = require('../models/models');
 
 router.get('/---', (req, res) => {
-	models.gallery.find((err, docs) => {
-		let galleries = docs;
-
-		models.design.find((err, docs) => {
-			let designs = docs;
-
-			models.info_text.find((err, docs) => {
-				let info = docs;
+	models.gallery.find((err, galleries) => {
+		models.design.find((err, designs) => {
+			models.info_text.find((err, info) => {
 				res.render('settings', { title: "Settings", pagename: "settings", docs: { galleries, designs, info } })
 			})
 		})
@@ -32,14 +27,13 @@ router.post('/save/gallery', (req, res) => {
 
 	if (req.body.bulk) {
 		let bulk = req.body.bulk.split("\n");
-		obj = bulk.map(gallery => { let e = gallery.split(" -- "); return {key: e[0], set_ID: e[1], label: e[2]} });
+		obj = bulk.map(gallery => { let e = gallery.split(" -- "); return {key: e[0].trim(), set_ID: e[1].trim(), label: e[2].trim()} });
 		complete(obj);
 	} else {
 		obj = { key: req.body.key, set_ID: req.body.set_ID, label: req.body.label };
 		complete(obj);
 	}
 });
-
 
 // router.post('/save/:setting', (req, res) => {
 // 	switch (req.params.setting) {
@@ -68,12 +62,9 @@ router.post('/save/gallery', (req, res) => {
 // });
 
 router.post('/delete/gallery/', (req, res) => {
+	var query = req.body.gallery_to_delete === "*" ? {} : {_id: req.body.gallery_to_delete};
 	var cb = err => err ? res.send(err) : res.redirect(req.get("referrer"));
-	if (req.body.gallery_to_delete.toLowerCase() === "all") {
-		models.gallery.deleteMany({}, cb);
-	} else {
-		models.gallery.deleteOne({_id: req.body.gallery_to_delete}, cb);
-	}
+	models.gallery.deleteMany(query, cb);
 });
 
 module.exports = router;
