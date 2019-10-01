6a79f2672f6fb0c2052423703457c1c354d265c1
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2019-10-01 18:08:35 +0100

    function for changing index fields of each document in galleries collection after insertion/deletion queries

--- routes/settings.js
+++ routes/settings.js
@@ -2,9 +2,19 @@ var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
 var models = require('../models/models');
+var indexShift = (currentDoc, deletion, cb) => {
+	models.gallery.find({index: {$gte: currentDoc.index}}).sort({index: 1}).exec((err, docs) => {
+		if (err) return err;
+		docs.forEach(doc => {
+			doc.index += (deletion ? -1 : 1);
+			doc.save();
+		});
+		if (cb) cb();
+	})
+};
 
 router.get('/---', (req, res) => {
-	models.gallery.find({index: {$gte: 0}}).sort({index: 1}).exec((err, galleries) => {
+	models.gallery.find().sort({index: 1}).exec((err, galleries) => {
 		models.design.find((err, designs) => {
 			models.info_text.find((err, info) => {
 				res.render('settings', { title: "Settings", pagename: "settings", docs: { galleries, designs, info: info[0] } })
@@ -13,7 +23,7 @@ router.get('/---', (req, res) => {
 	})
 });
 
-router.post('/save/gallery', (req, res) => {
+router.post('/gallery/save', (req, res) => {
 	var obj;
 
 	function complete (obj) {
@@ -29,27 +39,36 @@ router.post('/save/gallery', (req, res) => {
 			let e = gallery.split(" -- ");
 			return {tag: e[0].trim(), set_id: e[1].trim(), label: e[2].trim(), index: e[3]}
 		});
+		bulk.forEach(item => indexShift(item, 0));
 		complete(bulk);
 	} else {
 		obj = { tag: req.body.tag, set_id: req.body.set_id, label: req.body.label, index: req.body.index };
-		complete(obj);
+		indexShift(obj, 0, () => complete(obj));
 	}
 });
 
-router.post('/delete/gallery/', (req, res) => {
-	var query = req.body.gallery_to_delete === "*" ? {} : {_id: req.body.gallery_to_delete};
+router.post('/gallery/delete', (req, res) => {
+	var all = req.body.gallery_to_delete === "*";
+	var query = {_id: req.body.gallery_to_delete};
 	var cb = err => err ? res.send(err) : res.redirect(req.get("referrer"));
-	models.gallery.deleteMany(query, cb);
+
+	if (!all) {
+		models.gallery.findOne(query, (err, doc) => {
+			indexShift(doc, 1, () => models.gallery.deleteOne(query, cb));
+		})
+	} else {
+		models.gallery.deleteMany({}, cb);
+	}
 });
 
-router.post('/save/info-text/', (req, res) => {
+router.post('/info-text/save', (req, res) => {
 	models.info_text.deleteMany({}, err => {
 		var newInfo = new models.info_text({ text: req.body.text });
 		newInfo.save(err => err ? res.send(err) : res.redirect(req.get("referrer")));
 	});
 });
 
-router.post('/save/design/', (req, res) => {
+router.post('/design/save', (req, res) => {
 	var newDesign = new models.design({
 		d_id: req.body.d_id,
 		text: {
@@ -62,10 +81,18 @@ router.post('/save/design/', (req, res) => {
 	newDesign.save(err => err ? res.send(err) : res.redirect(req.get("referrer")));
 });
 
-router.post('/delete/design/', (req, res) => {
-	var query = req.body.design_to_delete === "*" ? {} : {_id: req.body.design_to_delete};
+router.post('/design/delete', (req, res) => {
+	var all = req.body.design_to_delete === "*";
+	var query = {_id: req.body.design_to_delete};
 	var cb = err => err ? res.send(err) : res.redirect(req.get("referrer"));
-	models.design.deleteMany(query, cb);
+
+	if (!all) {
+		models.design.findOne(query, (err, doc) => {
+			indexShift(doc, 1, () => models.design.deleteOne(query, cb));
+		})
+	} else {
+		models.design.deleteMany({}, cb);
+	}
 });
 
 module.exports = router;
