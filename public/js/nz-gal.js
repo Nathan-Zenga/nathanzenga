1d747b6711670d921f17a9069dde88ddfdc45f0e
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2019-11-11 17:24:17 +0000

    refactoring onload event handler for images

--- public/js/nz-gal.js
+++ public/js/nz-gal.js
@@ -1,7 +1,7 @@
 $(function() {
 	Galleria.loadTheme("https://cdnjs.cloudflare.com/ajax/libs/galleria/1.5.7/themes/classic/galleria.classic.min.js").configure({ transition: 'fade', showInfo: false, thumbnails: false })
-	.on("loadfinish", function() {
-		if (this._target.className === "inner img") {
+	.on("loadfinish", function(e) {
+		if (/inner|img/.test(this._target.className) && !e.cached) {
 			$(this._target).append("<div class='expand-icon'><i class='fas fa-expand'></i></div>").find(".expand-icon").fadeTo(0, 1).delay(2000).fadeOut(function() {
 				$(this).removeAttr("style")
 			});
@@ -33,7 +33,7 @@ $(function() {
 				break;
 
 			case "photo":
-				let set = id.split("-")[1];
+				var set = id.split("-")[1];
 				options.flickr = "set:" + set;
 				break;
 
@@ -45,14 +45,16 @@ $(function() {
 		$("#gallery_view .iframe").galleria(options);
 	}
 
-	function onImageLoad(e) {
-		let img = this;
-		let i = typeof e.data == "number" ? e.data : e.data.index(img);
-		let landscape = img.width > img.height;
-		let portrait = img.width < img.height;
-		let orientation = portrait ? "vertical" : landscape && (i % 2 == 0) ? "horizontal" : "";
+	function orientation(e) {
+		var img = e.currentTarget;
+		var imgContainer = $(img).closest(".img-container").get(0);
+		var selector = "."+imgContainer.className.replace(/ /g, ".");
+		var i = $(selector).index(imgContainer);
+		var landscape = img.width > img.height;
+		var portrait = img.width < img.height;
+		var dir = portrait ? "vertical" : landscape && (i % 2 == 0) ? "horizontal" : "";
 
-		$(img).parents(".img-container").addClass(orientation).fadeTo(.5, 1, function() { $(this).removeAttr("style") }).children(".inner.img").galleria({
+		$(imgContainer).addClass(dir).fadeTo(.5, 1, function() { $(this).removeAttr("style") }).children(".inner.img").galleria({
 			imageCrop: true,
 			showImagenav: false
 		});
@@ -66,7 +68,7 @@ $(function() {
 					$(".content.galleria-init .grid").append('<div class="img-container media-container" style="opacity: 0"><div class="inner img" data-toggle="modal" data-target="#gallery_view" oncontextmenu="return false"><img src="'+ img.big +'"></div></div>');
 					if (i === data.length-1) $(".content.galleria-init .img").click(onclick);
 				});
-				$(".inner.img img").on("load", $(".inner.img img"), onImageLoad);
+				$(".inner.img img").on("load", orientation);
 			});
 		});
 	} else {
@@ -78,7 +80,7 @@ $(function() {
 			if (pagename === "photo") {
 				flickr.tags("nz-"+ id[0] +"-cover", function(data) {
 					if (data.length) {
-						$(img).html("<img src="+ data[0].big +">").children().on("load", i, onImageLoad);
+						$(img).html("<img src="+ data[0].big +">").children().on("load", orientation);
 					}
 				});
 			}
