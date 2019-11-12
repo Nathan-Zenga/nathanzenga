7e9f79c5bb04e6199c89a482491f945778446754
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2019-11-12 00:02:34 +0000

    new collection for admin password storage

--- models/models.js
+++ models/models.js
@@ -1,13 +1,14 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-module.exports.gallery = mongoose.model('Gallery', mongoose.Schema({
+module.exports.gallery = mongoose.model('Gallery', Schema({
 	tag: String,
 	set_id: String,
 	label: String,
 	index: Number
 }));
 
-module.exports.design = mongoose.model('Design', mongoose.Schema({
+module.exports.design = mongoose.model('Design', Schema({
 	d_id: { type: String, uppercase: true },
 	text: {
 		client: { type: String },
@@ -18,8 +19,14 @@ module.exports.design = mongoose.model('Design', mongoose.Schema({
 	index: Number
 }));
 
-module.exports.info_text = mongoose.model('Info_text', mongoose.Schema({
+module.exports.info_text = mongoose.model('Info_text', Schema({
 	text: String
+}, {
+	capped: { max: 1, size: 1000 }
+}));
+
+module.exports.admin = mongoose.model('Admin', Schema({
+	pass: String
 }, {
 	capped: { max: 1, size: 1000 }
 }));
