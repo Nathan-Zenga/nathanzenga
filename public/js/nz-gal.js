2922fca59a6189f23b4bd77e8baff3f862d8ce08
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2019-12-18 20:00:45 +0000

    fixing auto-placement/alignment of last horizontal image

--- public/js/nz-gal.js
+++ public/js/nz-gal.js
@@ -1,16 +1,15 @@
 $(function() {
-	Galleria.loadTheme("https://cdnjs.cloudflare.com/ajax/libs/galleria/1.5.7/themes/classic/galleria.classic.min.js").configure({ transition: 'fade', showInfo: false, thumbnails: false })
+	Galleria.loadTheme("https://cdnjs.cloudflare.com/ajax/libs/galleria/1.5.7/themes/classic/galleria.classic.min.js").configure({ transition: 'fade', showInfo: false, thumbnails: false, showImagenav: false })
 	.on("loadfinish", function(e) {
 		var target = this._target;
+		var $imgContainer = $(target).parents(".img-container");
 		if (/inner|img/.test(target.className) && !e.cached) {
 			$(target).append("<div class='expand-icon'><i class='fas fa-expand'></i></div>").find(".expand-icon").fadeTo(0, 1).delay(2500).fadeOut(function() {
 				$(this).removeAttr("style")
 			});
 
-			var $imgContainer = $(target).parents(".img-container");
-			if (parseInt($imgContainer.css("opacity")) === 0) {
-				$imgContainer.fadeTo(500, 1, function() { $(this).removeAttr("style") })
-			}
+			var opacity = Number(parseInt($imgContainer.css("opacity")) === 0);
+			$imgContainer.fadeTo(500, opacity, function() { $(this).removeAttr("style") })
 		}
 	});
 
@@ -19,6 +18,7 @@ $(function() {
 	var post = $.post('/key');
 	var key = post.status != 404 ? post.responseText : undefined;
 	var flickr = new Galleria.Flickr(key);
+	var counter = 0;
 
 	$(".content.galleria-init").prepend(!$(".inner.img").length ? "<div class='loader' style='position: relative; top: auto: left: auto;'><div class='spinner'></div></div>" : undefined);
 
@@ -55,16 +55,17 @@ $(function() {
 	function orientation(e) {
 		var img = e.currentTarget;
 		var imgContainer = $(img).closest(".img-container").get(0);
-		var selector = "."+imgContainer.className.replace(/ /g, ".");
-		var i = $(selector).index(imgContainer);
+		var i = $(".img-container").index(imgContainer);
 		var landscape = img.width > img.height;
 		var portrait = img.width < img.height;
 		var dir = portrait ? "vertical" : landscape && (i % 2 == 0) ? "horizontal" : "";
+		var finalLoad = counter === e.data.imgLength-1;
+		$(imgContainer).addClass(dir);
+		if(finalLoad && $(".horizontal:last").offset().top > $(".img-container:last").offset().top)
+			$(".horizontal:last").removeClass("horizontal");
+		counter += 1;
 
-		$(imgContainer).addClass(dir).children(".inner.img").galleria({
-			imageCrop: true,
-			showImagenav: false
-		});
+		$(imgContainer).children(".inner.img").galleria({ imageCrop: true });
 	}
 
 	if ($(".grid").length && !("grid" in document.body.style)) {
@@ -78,19 +79,20 @@ $(function() {
 	if (!$(".inner.img").length) {
 		var set = pagename === "artwork" ? "72157646703093220" : "72157647107363402";
 		flickr.set(set, function(data) {
+			var imgLength = data.length;
 			$(".loader").fadeOut(function() {
 				$(this).remove();
-				data = data.filter(function(img) { return !/web|mobile/.test(img.title) });
 				data.forEach(function(img, i) {
 					var selector = ".content.galleria-init" + ( $(".grid").length ? " .grid" : "" );
 					var colClassName = !$(".grid").length ? " col-sm-6" : "";
 					$(selector).append('<div class="img-container media-container'+ colClassName +'" style="opacity: 0"><div class="inner img" data-toggle="modal" data-target="#gallery_view" oncontextmenu="return false"><img src="'+ img.big +'"></div></div>');
 					if (i === data.length-1) $(".content.galleria-init .img").click({set}, loadGalleryView);
 				});
-				$(".inner.img img").on("load", orientation);
+				$(".inner.img img").on("load", {imgLength}, orientation);
 			});
 		});
 	} else {
+		var imgLength = $(".content.galleria-init .img").length;
 		$(".content.galleria-init .img").each(function(i) {
 			var img = this;
 			var id = img.id.split("-");
@@ -98,7 +100,7 @@ $(function() {
 
 			if (pagename === "photo") {
 				flickr.tags("nz-"+ id[0] +"-cover", function(data) {
-					if (data.length) $(img).html("<img src="+ data[0].big +">").children().on("load", orientation);
+					if (data.length) $(img).html("<img src="+ data[0].big +">").children().on("load", {imgLength}, orientation);
 				});
 			}
 			else if (pagename === "design") {
@@ -106,7 +108,6 @@ $(function() {
 					imageCrop: !isSlideshow,
 					flickr: "tags:nz-designs-" + img.id,
 					flickrOptions: { imageSize: isSlideshow ? "original" : imageSize, max: isSlideshow ? undefined : 1 },
-					showImagenav: false,
 					show: img.id ? 0 : i,
 					autoplay: isSlideshow ? 4500 : undefined,
 					transitionSpeed: isSlideshow ? 1000 : undefined,
