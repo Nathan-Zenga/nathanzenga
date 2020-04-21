131cf335faaa0f001e0f4c8355da5d5905d55b56
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2020-04-21 22:11:29 +0100

    major refactoring for loading Galleria instances

---
+++ public/js/galleria-init.js
@@ -0,0 +1,67 @@
+Galleria.loadTheme("https://cdnjs.cloudflare.com/ajax/libs/galleria/1.5.7/themes/classic/galleria.classic.min.js").configure({ transition: 'fade', showInfo: false, thumbnails: false, showImagenav: false })
+.on("loadfinish", function(e) {
+    var target = this._target;
+    var $imgContainer = $(target).parents(".img-container");
+    if ($(".loader").length) $(".loader").hide(0).remove();
+    if (/inner|img/.test(target.className) && !e.cached) {
+        $(target).append("<div class='expand-icon'><i class='fas fa-expand'></i></div>").find(".expand-icon").fadeTo(0, 1).delay(2500).fadeOut(function() {
+            $(this).removeAttr("style")
+        });
+
+        var i = $(".img").index(target);
+        $imgContainer.delay(i * 100).fadeTo(500, 1, function() { $(this).removeAttr("style") })
+    }
+});
+
+var counter = 0;
+function imgOrientation(e) {
+    var img = e.currentTarget;
+    var imgContainer = $(img).closest(".img-container").get(0);
+    var i = $(".img-container").index(imgContainer);
+    var landscape = img.width > img.height;
+    var portrait = img.width < img.height;
+    var dir = portrait ? "vertical" : landscape && (i % 2 == 0) ? "horizontal" : "";
+    var finalLoad = counter === e.data.imgLength-1;
+    $(imgContainer).addClass(dir);
+    if( finalLoad && $(".horizontal:last").offset().top > $(".img-container:last").offset().top )
+        $(".horizontal:last").removeClass("horizontal");
+    counter += 1;
+
+    if ( finalLoad ) $(".inner.img").galleria({ imageCrop: true });
+}
+
+function loadGalleryView(e) {
+    var set = e.data ? e.data.set : null;
+    var isSlideshow = this.className.includes("slideshow");
+    var i = $(".inner.img").index(this);
+    var id = this.id;
+    var options = {
+        flickrOptions: { imageSize: isSlideshow ? "original" : "big" }
+    };
+
+    switch(document.body.id) {
+        case "photo":
+            set = id.split("-")[1];
+            options.flickr = "set:" + set;
+            break;
+            
+        case "design":
+            options.flickr = "tags:nz-designs-" + id;
+            break;
+
+        default:
+            options.flickr = "set:" + set;
+            options.flickrOptions.max = $(".inner.img").length;
+            options.show = i;
+    }
+
+    $("#gallery_view .iframe").galleria(options);
+}
+
+if ($(".grid").length && !("grid" in document.body.style)) {
+    if ($(".img-container").length) {
+        $(".img-container").unwrap(".grid").unwrap(".grid-container").addClass("col-sm-6");
+    } else {
+        $(".grid, .grid-container").remove();
+    }
+}
--- public/js/main.js
+++ public/js/main.js
@@ -1,114 +1,73 @@
 $(function() {
-	var keys = {
-		27: 1, // esc
-		37: 1, // left
-		39: 1 // right
-	};
+    var keys = { 27: 1 /*esc*/, 37: 1 /*left*/, 39: 1 /*right*/ };
 
-	function smoothScroll(scrollTop) {
-		scrollTop = typeof scrollTop.data == "number" ? scrollTop.data : scrollTop;
-		$("html, body").stop().animate({ scrollTop }, 700, 'easeInOutExpo');
-	}
+    function smoothScroll(scrollTop) {
+        scrollTop = typeof scrollTop.data == "number" ? scrollTop.data : scrollTop;
+        $("html, body").stop().animate({ scrollTop }, 700, 'easeInOutExpo');
+    }
 
-	function toggleMenuFix() {
-		var isBelow = window.pageYOffset >= $("main").offset().top;
-		$(".menu").toggleClass("fixed", isBelow);
-	};
+    function toggleMenuFix() {
+        var isBelow = window.pageYOffset >= $("main").offset().top;
+        $(".menu").toggleClass("fixed", isBelow);
+    };
 
-	function toggleScrollTracker() {
-		$(".scroll-tracker").css({
-			width: (window.pageYOffset / (document.documentElement.offsetHeight-window.innerHeight) * 100) + "%"
-		})
-	};
+    function toggleScrollTracker() {
+        $(".scroll-tracker").css({
+            width: (window.pageYOffset / (document.documentElement.offsetHeight-window.innerHeight) * 100) + "%"
+        })
+    };
 
-	document.onkeydown = function(e) {
-		e = e || window.event;
-		if (keys[e.keyCode] && $("#gallery_view.in").length) {
-			if (e.preventDefault) e.preventDefault();
+    document.onkeydown = function(e) {
+        e = e || window.event;
+        if (keys[e.keyCode] && $("#gallery_view.in").length) {
+            if (e.preventDefault) e.preventDefault();
 
-			switch(e.keyCode) {
-				case 27:
-					$(".close-view").click();
-					break;
+            switch(e.keyCode) {
+                case 27:
+                    $(".close-view").click();
+                    break;
 
-				case 37:
-					$("#gallery_view .galleria-image-nav-left").click();
-					break;
+                case 37:
+                    $("#gallery_view .galleria-image-nav-left").click();
+                    break;
 
-				case 39:
-					$("#gallery_view .galleria-image-nav-right").click();
-					break;
-			}
+                case 39:
+                    $("#gallery_view .galleria-image-nav-right").click();
+                    break;
+            }
 
-			return false;
-		}
-	}
+            return false;
+        }
+    }
 
-	$(".menu-icon").click(function() {
-		if (!$(".menu :animated").length) {
-			$(this).toggleClass("is-active");
-			var menuOpen = $(this).hasClass("is-active");
-			var menuFixed = $(".menu").hasClass("fixed");
-			var posToggle   = () => $(".nav-group").toggleClass("pos-toggle", menuOpen);
-			var widthToggle = () => $(".nav-group").toggleClass("width-toggle", menuOpen);
-			var navToggle   = () => $("nav").slideToggle(function() { if ($(this).is(":hidden")) $(this).css("display", "") });
-			var steps = [ posToggle, widthToggle, navToggle ];
-			steps = menuOpen ? steps : steps.reverse();
-			steps.forEach((step, i) => { setTimeout(() => step(), menuFixed ? 400 * i : 0) });
-		}
-	});
+    $(".menu-icon").click(function() {
+        if (!$(".menu :animated").length) {
+            $(this).toggleClass("is-active");
+            var menuOpen = $(this).hasClass("is-active");
+            var menuFixed = $(".menu").hasClass("fixed");
+            var posToggle   = () => $(".nav-group").toggleClass("pos-toggle", menuOpen);
+            var widthToggle = () => $(".nav-group").toggleClass("width-toggle", menuOpen);
+            var navToggle   = () => $("nav").slideToggle(function() { if ($(this).is(":hidden")) $(this).css("display", "") });
+            var steps = [ posToggle, widthToggle, navToggle ];
+            steps = menuOpen ? steps : steps.reverse();
+            steps.forEach((step, i) => { setTimeout(() => step(), menuFixed ? 400 * i : 0) });
+        }
+    });
 
-	$(".toTop").click(0, smoothScroll);
+    $(".toTop").click(0, smoothScroll);
 
-	toggleMenuFix();
-	toggleScrollTracker();
-	$(window).scroll(toggleMenuFix);
-	$(window).on("scroll resize", toggleScrollTracker);
+    $(window).on("load scroll", toggleMenuFix);
+    $(window).on("load scroll resize", toggleScrollTracker);
 
-	$("#info form").submit(function(e) {
-		e.preventDefault();
-		var form = this;
-		var data = {};
+    $("#info form").submit(function(e) {
+        e.preventDefault();
+        var form = this;
+        $(form).append("<div class='loader' style='display:none'><div class='spinner'></div></div>").find(".loader").stop().fadeIn();
 
-		$(this).serializeArray().forEach(function(field) {
-			var name = field.name;
-			data[name] = field.value;
-		});
-
-		$(this).append("<div class='loader' style='display:none'><div class='spinner'></div></div>").find(".loader").stop().fadeIn();
-
-		$.post("/send/message", data, function(res) {
-			$(".loader").stop().fadeOut();
-			if (res.includes("sent")) $(form).find(".details").val("");
-			alert(res);
-		})
-	});
-
-	if (document.body.id === "settings") {
-		$("#bulk").change(function() {
-			var isRequired = !this.value.trim();
-			$(this).closest("form").find(".details").not(this).attr("required", isRequired);
-		});
-
-		$.post("/settings/design/documents", function(docs) {
-			$("form.design-edit-settings select").change(function() {
-				var $form = $(this).parents("form");
-				var id = this.value;
-				var doc = docs.filter(function(doc) { return doc._id == id })[0];
-				if (!doc) {
-					$form.find(".details").val("");
-				} else {
-					$.each(doc, function(k, v) {
-						if (typeof v !== "object") {
-							$form.find(".details[name="+ k +"]").val(v);
-						} else {
-							$.each(v, function(k2, v2) {
-								$form.find(".details[name="+ k2 +"]").val(v2);
-							})
-						}
-					})
-				}
-			});
-		})
-	}
+        $.post("/send/message", $(this).serializeArray(), function(res) {
+            $(".loader").stop().fadeOut();
+            if (res.includes("sent")) $(form).find(".details").val("");
+            alert(res);
+        })
+    });
 });
