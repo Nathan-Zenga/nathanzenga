51b18d830b5869ca0c4bea72bf39132b5b9ee34e
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2020-05-20 10:52:38 +0100

    increasing request body size limit

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ var app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: '20mb' }));
+app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }));
 app.use(cookieParser());
 
 // Set static folder
