d1d04ecbabc6fa15370e598c49148760f3739d11
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2020-05-26 00:30:22 +0100

    Enabled process for manually setting new photo set image cover

--- routes/settings.js
+++ routes/settings.js
@@ -186,6 +186,24 @@ router.post('/photo/set/sort-order', (req, res) => {
     })
 });
 
+router.post('/photo/set/cover', (req, res) => {
+    var { id, photo_set } = req.body;
+    if (!id || !photo_set) return res.send("Required field(s) missing");
+    Photo.findOne({ photo_set, photo_set_cover: true }, (err, oldCover) => {
+        Photo.findById(id, (err2, newCover) => {
+            if (err || err2) return console.error(err || err2), res.send("Error occurred whilst fetching image");
+            if (!oldCover || !newCover) return res.send("Old / new cover image not found");
+            newCover.photo_set_cover = true;
+            newCover.photo_set_index = oldCover.photo_set_index;
+            newCover.save();
+            oldCover.photo_set_cover = false;
+            oldCover.photo_set_index = undefined;
+            oldCover.save();
+            res.send(`Cover image updated for ${photo_set} set`);
+        })
+    })
+});
+
 router.post('/info-text/save', (req, res) => {
     Info_text.deleteMany({}, err => {
         var newInfo = new Info_text({ text: req.body.text });
