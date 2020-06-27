1572ea78af6bc081d69bce3f011f4dabe7a0d1f6
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2020-06-27 04:21:41 +0100

    main server code cleanup: setting variables as constants, syntax changes, preserving lines

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var http = require('http'); // core module
-var path = require('path'); // core module
-var mongoose = require('mongoose');
-var session = require('express-session');
-var MemoryStore = require('memorystore')(session);
-var production = (process.env.NODE_ENV === "production");
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const http = require('http'); // core module
+const path = require('path'); // core module
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MemoryStore = require('memorystore')(session);
+const production = (process.env.NODE_ENV === "production");
 
 mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true });
-
-let conn = mongoose.connection;
-conn.once('open', function() {
-    console.log("Connected to DB")
-});
-
-var app = express();
+mongoose.connection.once('open', () => { console.log("Connected to DB") });
 
 // View Engine
 app.set('view engine', 'ejs');
@@ -39,7 +34,7 @@ app.use(session({
 }));
 
 // Global variables
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
     res.locals.production = production;
     res.locals.url = req.originalUrl;
     next();
@@ -53,12 +48,8 @@ app.get("*", (req, res) => {
 });
 
 // Set port + listen for requests
-var port = process.env.PORT || 5678;
-app.listen(port, function() {
+const port = process.env.PORT || 5678;
+app.listen(port, () => {
     console.log('Server started on port '+ port);
-    if (production) {
-        setInterval(function() {
-            http.get(process.env.URL);
-        }, 60000 * 25);
-    }
+    if (production) setInterval(() => { http.get(process.env.URL) }, 60000 * 25);
 });
