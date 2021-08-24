b61ac85d6e8ef7a9507d06410c7b6945a4ea64c1
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2021-08-24 04:04:14 +0100

    set up crossfade transition effect between pages

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,39 @@
+import { useEffect, useState } from 'react'
 import Meta from './Meta'
 import Header from './Header'
 import Footer from './Footer'
+import styles from '../styles/Layout.module.css';
 
 const Layout = ({ children }) => {
+  const [displayChildren, setDisplayChildren] = useState(children);
+  const [transitionStage, setTransitionStage] = useState("fadeOut");
+
+  useEffect(() => {
+    setTransitionStage("fadeIn")
+  }, []);
+
+  useEffect(() => {
+    if (children.type.name !== displayChildren.type.name) {
+      setTransitionStage("fadeOut");
+    }
+  }, [children, setDisplayChildren, displayChildren]);
+
   return (
     <>
-      <Meta />
-      <Header />
-      <main>
-        {children}
-      </main>
-      <Footer />
+    <Meta />
+    <Header />
+    <main
+      onTransitionEnd={() => {
+        if (transitionStage === "fadeOut") {
+          setDisplayChildren(children);
+          setTransitionStage("fadeIn");
+        }
+      }}
+      className={`${styles["main-content"]} ${styles[transitionStage]}`}
+    >
+      {displayChildren}
+    </main>
+    <Footer />
     </>
   )
 }
