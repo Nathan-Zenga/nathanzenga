bff2386be575d51a25ead2e0a158c7d56e35ab00
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2021-08-24 03:41:18 +0100

    setup Next.js sever and deployment configs

--- server.js
+++ server.js
@@ -1,31 +1,26 @@
 const express = require('express');
+const server = express();
 const next = require('next');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-
-const path = require('path'); // core module
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MemoryStore = require('memorystore')(session);
-
 const port = process.env.PORT || 5678;
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
-const server = express();
 
 mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connection.once('open', () => { console.log("Connected to DB") });
+mongoose.connection.once('open', _ => { console.log("Connected to DB") });
 
-
-server.use(bodyParser.json({ limit: '200mb' }));
-server.use(bodyParser.urlencoded({ limit: '200mb', extended: false }));
+server.use(bodyParser.json({ limit: "200mb" }));
+server.use(bodyParser.urlencoded({ limit: "200mb", extended: false }));
 server.use(cookieParser());
-
 
 // Express session
 server.use(session({
-  secret: 'secret',
+  secret: "secret",
   name: "session" + Math.round(Math.random()*1000000),
   saveUninitialized: true,
   resave: true,
@@ -40,19 +35,12 @@ server.use((req, res, next) => {
   next();
 });
 
-
 app.prepare().then(() => {
   server.use('/api/', require('./routes/index'));
   server.use('/api/settings', require('./routes/settings'));
   server.use('/api/settings/photo', require('./routes/photo-settings'));
   server.use('/api/settings/design', require('./routes/design-settings'));
-  
-  server.all('*', (req, res) => {
-    return handle(req, res);
-  });
-
-  server.listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  });
+  server.post('*', (req, res) => res.status(400).send("Sorry, your request currently cannot be processed"));
+  server.all('*', (req, res) => handle(req, res));
+  server.listen(port, () => { console.log(`Server started ${!dev ? "" : "on port " + port}`) });
 })
