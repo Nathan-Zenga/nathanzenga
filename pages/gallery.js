9c150708d84ed79936c20ed2d3985f6d0ed5e144
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2021-09-05 01:38:53 +0100

    enable redirect for missing galleries
    keep position value within range of number of gallery items

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,23 +1,45 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { motion } from "framer-motion";
 import Meta from '../components/Meta';
 import GalleryImage from '../components/GalleryCarouselImage';
 import { getPhotos } from '../services/fetchData';
 
-const Gallery = ({ photos, position, set = "Missing Gallery" }) => {
+const Gallery = ({ photos, position }) => {
   const { title, ogTitle } = Meta.defaultProps;
+  const set = photos.length ? photos[0].photo_set : "Missing Gallery";
+  const router = useRouter();
+
+  useEffect(() => { photos.length || router.replace("/404") }, [photos]);
+
+  const showFirst = (index, array) => {
+    let pos = !isNaN(position) ? parseInt(position) : 0;
+    let showFirst = (index+1) === pos;
+    showFirst = showFirst || (index == 0 && pos < 1);
+    showFirst = showFirst || (index == array.length-1 && pos > array.length);
+    return showFirst;
+  }
+
   return (
     <>
     <Meta title={`${set} - ${title}`} ogTitle={`${set} - ${ogTitle}`} />
-    <div className="container" style={{ padding: 0 }}>
+    <motion.div
+      className="container"
+      style={{ padding: 0 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
       <div id="gallery-carousel" className="carousel slide carousel-fade" onContextMenu={() => false}>
         <ol className="carousel-indicators">{
-          photos.map((p, i) => {
-            const active = (i+1) === parseInt(position) ? "active" : "";
+          photos.map((p, i, a) => {
+            const active = showFirst(i, a) ? "active" : "";
             return <li key={"image-" + i} data-target="#gallery-carousel" data-slide-to={i} className={active}></li>
           })
         }</ol>
 
         <div className="carousel-inner">{
-          photos.map((p, i) => <GalleryImage key={p._id} active={(i+1) === parseInt(position)} image={p} />)
+          photos.map((p, i, a) => <GalleryImage key={p._id} active={showFirst(i, a)} image={p} />)
         }</div>
 
         <a className="carousel-control-prev" href="#gallery-carousel" role="button" data-slide="prev">
@@ -30,14 +52,14 @@ const Gallery = ({ photos, position, set = "Missing Gallery" }) => {
           <span className="sr-only">Next</span>
         </a>
       </div>
-    </div>
+    </motion.div>
     </>
   );
 }
 
 export const getServerSideProps = async ctx => {
   const photos = await getPhotos({ photo_set: ctx.query.set, sort: '{ "index": 1 }' });
-  return { props: { photos, position: ctx.query.image || 1, set: (photos[0] || {}).photo_set } };
+  return { props: { photos, position: ctx.query.image || 1 } };
 }
 
 export default Gallery;
