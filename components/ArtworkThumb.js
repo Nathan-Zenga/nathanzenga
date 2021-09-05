f09f775ff25182a93b561807a5b0bf48da0627ee
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2021-09-05 04:38:58 +0100

    control animation of images during onLoad events

--- components/ArtworkThumb.js
+++ components/ArtworkThumb.js
@@ -1,47 +1,47 @@
 import Link from 'next/link';
-import React, { Component } from 'react';
-import { motion } from "framer-motion";
+import { useEffect, useState } from 'react';
+import { motion, useAnimation } from "framer-motion";
 
-class ArtworkThumb extends Component {
+const ArtworkThumb = ({ image, index }) => {
 
-  constructor(props) {
-    super(props);
-    this.state = { ...props, imageObjectURL: "" };
-  }
+  const [ imageObjectURL, setImageObjectURL ] = useState("");
+  const animation = useAnimation();
 
-  async componentDidMount() {
-    const { image } = this.state;
+  useEffect(async () => {
+    if (imageObjectURL !== "") return;
     const img = new Image();
     const blob = await fetch(image.photo_url).then(res => res.blob());
     const objURL = URL.createObjectURL(blob);
     img.onload = e => URL.revokeObjectURL(e.target.src);
-    this.setState({ imageObjectURL: objURL });
+    setImageObjectURL(objURL);
     setTimeout(() => { img.src = objURL }, 100);
-  }
+  }, [imageObjectURL]);
 
-  render() {
-    const { image, imageObjectURL, index } = this.state;
-    const imageContainerClass = "img-container media-container col-sm-6 float-left";
-    const query = { set: image.photo_set.toLowerCase() };
-    if (!isNaN(index)) query.image = index + 1;
-    return (
-      <motion.div
-        className={imageContainerClass}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: .5 }}
-      >
-        <Link href={{ pathname: "/gallery", query }}>
-          <a className="inner img"
-             id={image.photo_set}
-             onContextMenu={e => false}>
-            <img src={imageObjectURL} style={{ width: "100%" }} />
-          </a>
-        </Link>
-      </motion.div>
-    )
-  }
+  const imageContainerClass = "img-container media-container col-sm-6 float-left";
+  const query = { set: image.photo_set.toLowerCase() };
+  if (!isNaN(index)) query.image = index + 1;
+  return (
+    <motion.div
+      className={imageContainerClass}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: .5 }}
+    >
+      <Link href={{ pathname: "/gallery", query }}>
+        <a className="inner img"
+            id={image.photo_set}
+            onContextMenu={e => false}>
+          <motion.img
+            src={imageObjectURL}
+            style={{ width: "100%", opacity: 0 }}
+            animate={animation}
+            onLoad={async e => { await animation.start({ opacity: 1 }) }}
+          />
+        </a>
+      </Link>
+    </motion.div>
+  )
 }
 
 export default ArtworkThumb;
