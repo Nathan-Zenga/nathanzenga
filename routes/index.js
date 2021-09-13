3af75ab0c63b6c45a46d388af1b56bdae03654e4
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2021-09-13 15:28:29 +0100

    Revert "migrate router handler code to fetchData file"
    This reverts commit 3557e56671e81b828da1381c3cd1bbf53dac2f5b.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,27 @@ const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
 const { OAuth2 } = (require("googleapis")).google.auth;
+const { Design, Info_text, Photo } = require('../models/models');
 const { OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET, OAUTH_REFRESH_TOKEN } = process.env;
+
+router.get('/designs/-', async (req, res) => {
+    const { tools } = req.query;
+    const filter = Object.assign({ hidden: false }, tools ? { "text.tools": new RegExp(tools, "i") } : {});
+    const designs = await Design.find(filter).sort({ index: 1 }).exec();
+    const design_docs = await Photo.find({ photo_set: /^design-/ }).sort({ photo_set: 1, orientation: 1, index: 1 }).exec();
+    res.send({ designs, design_docs });
+});
+
+router.get('/info/-', (req, res) => {
+    Info_text.findOne((err, txt) => res.send(txt))
+});
+
+router.post('/p', async (req, res) => {
+    const qry = { ...req.body, sort: undefined };
+    if (qry.photo_set) qry.photo_set = { $regex: RegExp(`^${qry.photo_set}$`, "i") };
+    const photos = await Photo.find(qry).sort(JSON.parse(req.body.sort || "{}")).exec();
+    res.send(photos);
+});
 
 router.post('/send/message', async (req, res) => {
     const { name, email, subject, message } = req.body;
