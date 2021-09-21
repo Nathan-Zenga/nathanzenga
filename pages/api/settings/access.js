9a54e79eed4910f2bd5443e7692249343fad1982
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2021-09-21 23:15:57 +0100

    ensure login API route resolves without stalling requests

--- pages/api/settings/access.js
+++ pages/api/settings/access.js
@@ -1,13 +1,16 @@
 const passport = require('../../../config/passport');
 
-export default function handler(req, res) {
-  req.body.username = "admin";
-  passport.authenticate("admin-login", { session: true }, (err, user, info) => {
-    if (err) return res.status(500).send(err.message || err);
-    if (!user) return res.status(400).send(info.message);
-    req.login(user, err => {
-      if (err) return res.status(500).send(err.message || err);
-      res.send("/settings/---");
-    });
-  })(req, res);
+export default async function handler(req, res) {
+  return new Promise((resolve, reject) => {
+    req.body.username = "admin";
+    passport.authenticate("admin-login", { session: true }, (err, user, info) => {
+      if (err) return res.status(500).send(err.message || err), reject();
+      if (!user) return res.status(400).send(info.message), reject();
+      req.login(user, err => {
+        if (err) return res.status(500).send(err.message || err), reject();
+        res.send("/settings/---");
+        resolve();
+      });
+    })(req, res);
+  })
 }
