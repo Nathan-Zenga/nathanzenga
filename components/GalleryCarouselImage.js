b731ce03191a7f65df1c263fed6f2630c3a7cb5d
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2021-11-29 19:38:43 +0000

    convert remaining class components to function components

--- components/GalleryCarouselImage.js
+++ components/GalleryCarouselImage.js
@@ -1,30 +1,19 @@
-import React, { Component } from 'react';
+import { useEffect, useState } from "react";
 
-class GalleryCarouselImage extends Component {
+const GalleryCarouselImage = ({ image, active })  => {
 
-  constructor(props) {
-    super(props);
-    this.state = { ...props, imageObjectURL: "" };
-  }
+  const [ imageObjectURL, setObjURL ] = useState("");
 
-  async componentDidMount() {
-    const { image } = this.state;
+  useEffect(async () => {
     const img = new Image();
     const blob = await fetch(image.photo_url).then(res => res.blob());
     const objURL = URL.createObjectURL(blob);
     img.onload = e => URL.revokeObjectURL(e.target.src);
-    this.setState({ imageObjectURL: objURL });
+    setObjURL(objURL);
     setTimeout(() => { img.src = objURL }, 100);
-  }
+  }, []);
 
-  render() {
-    const { active, imageObjectURL: url } = this.state;
-    return (
-      <div className={"carousel-item " + (active ? "active" : "")}>
-        <img src={url} />
-      </div>
-    )
-  }
+  return <div className={`carousel-item${active ? " active" : ""}`}><img src={imageObjectURL} /></div>
 }
 
 export default GalleryCarouselImage;
