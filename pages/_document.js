535e0d61a912b6ab0abd777ff761b2e712cda8f9
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2022-04-13 21:01:56 +0100

    convert + import defaults css stylesheet as scss

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,6 @@ class MyDocument extends Document {
     return (
       <Html lang="en" id="nz">
         <Head>
-          <link rel="stylesheet" type="text/css" href="/css/defaults.css" />
           <link rel="stylesheet" type="text/css" href="/css/main.css" />
           <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
