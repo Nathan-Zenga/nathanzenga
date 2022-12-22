038622843821a2f72b29ebe2499550032621bbec
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2022-12-22 14:42:22 +0000

    style labels to appear underneath images

--- components/Footer.js
+++ components/Footer.js
@@ -2,12 +2,10 @@ const Footer = () => {
   return (
     <>
     <footer>
-      <div className="container">
-        <div className="socials">
-          <a className="fab fa-instagram" target="_blank" aria-hidden="true" href="http://instagram.com/nathanzenga" />
-          <a className="fab fa-linkedin" target="_blank" aria-hidden="true" href="https://uk.linkedin.com/in/nathan-zengamambu" />
-          <a className="fal fa-envelope" target="_blank" aria-hidden="true" href="mailto:nathanzenga@gmail.com" />
-        </div>
+      <div className="socials container">
+        <a className="fab fa-instagram" target="_blank" aria-hidden="true" href="http://instagram.com/nathanzenga" />
+        <a className="fab fa-linkedin" target="_blank" aria-hidden="true" href="https://uk.linkedin.com/in/nathan-zengamambu" />
+        <a className="fal fa-envelope" target="_blank" aria-hidden="true" href="mailto:nathanzenga@gmail.com" />
       </div>
     </footer>
     <script src="/js/main.js" />
